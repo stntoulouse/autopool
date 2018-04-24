58c062fe6b80b3f1afa00ca9771f644463afce9e
stntoulouse <stntoulouse@gmail.com>
2018-04-24 13:46:35 -0400

    organisation des fichiers

--- interface.js
+++ interface.js
@@ -1,17 +1,1 @@
-// gestion du bouton ajouter un pooler
-document.getElementById('add-pooler').addEventListener("click", addPooler);
-document.getElementById('new-pooler').addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        addPooler();
-    }
-});
-
-function addPooler() {
-    let newPoolerName = document.getElementById('new-pooler').value;
-    if (newPoolerName != '') {
-        document.getElementById('new-pooler').value = '';
-        POOLERS_NAMES.push(newPoolerName);
-        let position = POOLERS_NAMES.length - 1;
-        document.getElementById('poolers-list').innerHTML += '<div class="pooler" id="pooler-' + position + '">' + newPoolerName + '<div id="players-of-' + position + '"></div></div>';
-    }
-}
+// Outils pour le deroulement du Pool
---
+++ param.js
@@ -0,0 +1,4 @@
+// Outils pour la preparation du pool :
+//     - Selection des poolers
+//     - Chargement des statistiques des joueurs
+//     - Bouton de lancement du pool
